35c0f79aa9234d80408d64fb1a2e2edf2f00b45c
Raiu1210 <baio1484@gmail.com>
2019-07-18 14:10:56 +0900

    deleted useless function

--- front_end/src/main.js
+++ front_end/src/main.js
@@ -3,13 +3,6 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
-async function mpurse_test() {
-  var address = await window.mpurse.getAddress()
-
-  window.mpurse.sendAsset('MWUKwMmfBZ8ZgaLxwUCgb14huzvbZNn87c', "MONA", 1.14514, 'plain', 'tamami-chan kawaii!')
-}
-
-// mpurse_test()
 
 new Vue({
   render: h => h(App),
